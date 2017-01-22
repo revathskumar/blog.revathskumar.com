854c004565a5125c67405265ea8cc203a531e3ac
Revath S Kumar <rsk@revathskumar.com>
2017-01-23 02:01:46 +0530

    gulp update and use gulp-clean-css

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,10 +1,10 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
-var uglify = require('gulp-minify-css');
+var cleanCSS = require('gulp-clean-css');
 
 gulp.task('css', function(){
   gulp.src(['./css/main.css', './css/syntax.css'])
-    .pipe(uglify())
+    .pipe(cleanCSS())
     .pipe(concat('blog.min.css'))
     .pipe(gulp.dest('./css/'));
 });
