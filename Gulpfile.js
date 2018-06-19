7eb277ce9e3533c40577c340ea89328537597647
Revath S Kumar <rsk@revathskumar.com>
2018-06-20 00:23:46 +0530

    chore(packages) : updated gulp and other packages

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,9 +2,10 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var cleanCSS = require('gulp-clean-css');
 
-gulp.task('css', function(){
+gulp.task('css', function(done){
   gulp.src(['./css/main.css', './css/syntax.css'])
     .pipe(cleanCSS())
     .pipe(concat('blog.min.css'))
     .pipe(gulp.dest('./css/'));
+  done();
 });
