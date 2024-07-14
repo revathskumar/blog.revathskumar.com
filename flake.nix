84a27e9e0a6ff8f12dc85aabe33622f45e92571d
Revath S Kumar <rsk@revathskumar.com>
2024-07-14 10:55:04 +0200

    chore: basic nix setup

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  inputs = {
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.2405.632874.tar.gz";
+    nixpkgs-ruby.url = "github:bobvanderlinden/nixpkgs-ruby";
+    nixpkgs-ruby.inputs.nixpkgs.follows = "nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, nixpkgs-ruby, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        rubyVersion = nixpkgs.lib.fileContents ./.ruby-version;
+        ruby = nixpkgs-ruby.lib.mkRuby { inherit pkgs rubyVersion; };
+
+        gems = pkgs.bundlerEnv {
+          name = "gemset";
+          inherit ruby;
+          gemfile = ./Gemfile;
+          lockfile = ./Gemfile.lock;
+          gemset = ./gemset.nix;
+          groups = [ "default" "production" "development" "test" ];
+        };
+      in {
+        devShell = with pkgs;
+          mkShell {
+            buildInputs = [
+              # gems
+              ruby
+              bundler
+              nodejs_22
+            ];
+          };
+      });
+}
