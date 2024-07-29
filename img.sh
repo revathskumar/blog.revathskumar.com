10f0b78127f7642993e05cd0c21500ea881eaa0e
Revath S Kumar <rsk@revathskumar.com>
2024-07-29 16:16:34 +0200

    chore: add script to convert images to webp

---
+++ img.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# depends on libwebp package
+
+directory="${1%/}"
+if command -v img2webp >/dev/null 2>&1 ; then
+    echo "img2webp found"
+    echo "version: $(img2webp -version)"
+else
+    echo "img2webp not found"
+    exit 1;
+fi
+
+for file in "$directory"/*.{png,jpg}
+do
+  echo "$file"
+  outfile="${file//.png/.webp}"
+  outfile="${file//.jpg/.webp}"
+  img2webp "$file" -o "$outfile"
+  if [ $? -eq 0 ]; then
+    echo "convertion: success!"
+  else
+    echo "convertion: failure!"
+  fi
+done
